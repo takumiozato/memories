714d595e80d18cf03c91e21d02295ebb8bea8e4b
ozato takumi <ozato@up-point.jp>
2019-01-28 11:24:57 +0900

    materialize.cssを導入

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// サムネイルのチップ画像
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.chips');
+	var instances = M.Chips.init(elems);
+});
+
+// MagicGridの定義
 let magicGrid = new MagicGrid({
 	container: "#photo-grid",
 	items: 6,
