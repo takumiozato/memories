b707446fd99688557fa9726a3cd35fc431048c75
ozato takumi <ozato@up-point.jp>
2019-01-29 10:45:45 +0900

    詳細ページのデータを定義

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ window.onload = function () {
 	if(document.getElementById("photo-grid")){
 		var magicGrid = new MagicGrid({
 			container: "#photo-grid",
-			items: 6,
+			items: photoData.length,
 			gutter: 15,
 			animate: false,
 		});
@@ -21,13 +21,51 @@ window.onload = function () {
 var photoData = [
 	{
 		id: 0,
+		image: 'images/photo/0.jpg',
 		title: 'サンセットビーチ',
 		content: '結婚式の開場前、空いた時間に立ち寄った、北谷のサンセットビーチ。この時、まだ4月なのに焼けるような暑さだった。北谷という土地は沖縄の中でも比較的外国人が多く、とくにこのサンセットビーチは、日光浴や、サイクリングをしている外国人を毎日見かける。リラックスした空気が流れる居心地の良いビーチ。'
 	},
 	{
 		id: 1,
+		image: 'images/photo/1.jpg',
 		title: '古宇利ビーチ',
-		content: '沖縄県北部にある、古宇利島のビーチ。沖縄本島から車で行けるビーチの中で個人的に一番綺麗。絵具を流し込んだかのような信じられないほどのエメラルドグリーンに息をのむ。'
+		content: '沖縄県北部にある、古宇利島のビーチ。沖縄本島から車で行けるビーチの中で一番綺麗だと思う。絵具を流し込んだかのようなエメラルドグリーンの海が目の前に広がる。'
+	},
+	{
+		id: 2,
+		image: 'images/photo/2.jpg',
+		title: 'ブセナの海中展望塔',
+		content: '海底から伸びている展望塔。塔に入って階段を降りると、窓から海中の様子を見ることができる。サンゴ礁や熱帯魚がたくさん泳いでいて、カクレクマノミを目の前で見ることもできる。'
+	},
+	{
+		id: 3,
+		image: 'images/photo/3.jpg',
+		title: 'フクギ並木',
+		content: '台風や潮風から守ってくれる、フクギ。陽射しからも守ってくれて、涼しくて気持ちが良い。'
+	},
+	{
+		id: 4,
+		image: 'images/photo/4.jpg',
+		title: '家から見える景色',
+		content: '線路が目の前にあって、朝から晩まで電車が走り続けているし、日当たりも良くないが、だんだん慣れてきて、今では結構好きな景色。天神祭の花火も見える。'
+	},
+	{
+		id: 5,
+		image: 'images/photo/5.jpg',
+		title: '中ノ島',
+		content: ''
+	},
+	{
+		id: 6,
+		image: 'images/photo/6.jpg',
+		title: '夜の大阪城',
+		content: '意味もなく大阪城の近くに住むことにこだわったのだが、本当に満足している。大きな公園があるので散歩するのにもちょうど良く、夏はキャンプ用のチェアーを置いて本を読んだり眠ったりしていた。夜はライトアップされてて綺麗だし、ランナーがたくさんいて賑やか。'
+	},
+	{
+		id: 7,
+		image: 'images/photo/7.jpg',
+		title: '駅のホームにいた鳥',
+		content: 'どこかの駅で電車を待っている時に現れた鳥。沖縄ではあまり見かけないけど、大阪ではやたらと見かける。なんだか上品で綺麗。'
 	},
 ]
 
@@ -72,24 +110,21 @@ var userChip = {
 }
 
 var photoGrid = {
+	props: ['data'],
 	template: `
 		<transition name="photo-fade">
 			<div id="photo-grid" v-show="isShow">
-				<div class="photo-item"><router-link to="/detail"><img class="z-depth-2" src="images/photo/1.jpg" alt="イメージ画像"></router-link></div>
-				<div class="photo-item"><img class="z-depth-2" src="images/photo/2.jpg" alt="イメージ画像"></div>
-				<div class="photo-item"><img class="z-depth-2" src="images/photo/3.jpg" alt="イメージ画像"></div>
-				<div class="photo-item"><img class="z-depth-2" src="images/photo/4.jpg" alt="イメージ画像"></div>
-				<div class="photo-item"><img class="z-depth-2" src="images/photo/5.jpg" alt="イメージ画像"></div>
-				<div class="photo-item"><img class="z-depth-2" src="images/photo/6.jpg" alt="イメージ画像"></div>
-				<div class="photo-item"><img class="z-depth-2" src="images/photo/7.jpg" alt="イメージ画像"></div>
-				<div class="photo-item"><img class="z-depth-2" src="images/photo/8.jpg" alt="イメージ画像"></div>
-				<div class="photo-item"><img class="z-depth-2" src="images/photo/9.jpg" alt="イメージ画像"></div>
+				<div class="photo-item" v-for="photo in data">
+					<router-link :to="'/detail/' + photo.id">
+						<img class="z-depth-2" :src="photo.image" alt="イメージ画像">
+					</router-link>
+				</div>
 			</div>
 		</transition>
 	`,
 	data: function(){
 		return {
-			isShow: false
+			isShow: false,
 		}
 	},
 	mounted: function(){
@@ -102,9 +137,14 @@ var indexPage = {
 		<div>
 			<title-area></title-area>
 			<user-chip></user-chip>
-			<photo-grid></photo-grid>
+			<photo-grid :data="photoData"></photo-grid>
 		</div>
 	`,
+	data: function(){
+		return {
+			photoData: photoData
+		}
+	},
 	components: {
 		'title-area': titleArea,
 		'user-chip': userChip,
@@ -134,11 +174,11 @@ var iconWrapper = {
 }
 
 var detailImage = {
-	props: ['id'],
+	props: ['data'],
 	template: `
 		<transition name="image-fade">
 			<div class="detail-image-wrapper col m6 s12" v-show="isShow">
-				<img :src="'images/photo/' + id + '.jpg'" alt="イメージ画像" class="detail-image z-depth-2">
+				<img :src="data.image" alt="イメージ画像" class="detail-image z-depth-2">
 			</div>
 		</transition>
 	`,
@@ -178,7 +218,7 @@ var detailPage = {
 			<title-area></title-area>
 			<icon-wrapper></icon-wrapper>
 			<div class="row">
-				<detail-image :id="paramsId"></detail-image>
+				<detail-image :data="photoData[paramsId]"></detail-image>
 				<detail-content :data="photoData[paramsId]"></detail-content>
 			</div>
 		</div>
@@ -218,10 +258,6 @@ var router = new VueRouter({
 var vm = new Vue({
 	el: "#app",
 	router: router,
-	watch: {
-		// ルートが変更されたらこのメソッドを再び呼び出します
-		'$route': 'magicGridListen'
-	},
 	updated: function(){
 		this.magicGridListen();
 	},
@@ -230,7 +266,7 @@ var vm = new Vue({
 			if(document.getElementById("photo-grid")){
 				var magicGrid = new MagicGrid({
 					container: "#photo-grid",
-					items: 6,
+					items: photoData.length,
 					gutter: 15,
 					animate: false,
 				});
